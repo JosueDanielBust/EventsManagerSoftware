3468ec72e4a59fff68509489b90f6b2d22ca5324
nescob18 <nescob18@eafit.edu.co>
2016-05-20 03:19:16 -0500

    Merge origin/master
    Mejorasa

---
+++ EVS/src/MainInterfaces/DBAccess.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MainInterfaces;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public final class DBAccess {
+    
+private Connection conexion;
+    
+    public DBAccess(){
+        conectar();
+    }
+
+    public Connection getConexion() { return conexion; }
+    public void setConexion(Connection conexion) { this.conexion = conexion; }
+    public DBAccess conectar() {
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+            String DB = "jdbc:oracle:thin:@dbeafit.cyzd3byk9uno.us-east-1.rds.amazonaws.com:1521:DB20161";
+            conexion= DriverManager.getConnection(DB,"jbusta16","mlyBQM93");
+          
+            if(conexion!=null){
+                System.out.println("Conexion exitosa a esquema JBUSTA16");
+            } else {
+                System.out.println("Conexion fallida");
+            }
+        } catch(Exception e){ e.printStackTrace(); }
+    return this;
+    }
+    
+    public boolean ejecutar(String sql){
+     try{
+         Statement st = getConexion().createStatement(
+         ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+         st.executeUpdate(sql);
+         getConexion().commit();
+     }catch(SQLException e){
+         try {
+             conexion.rollback();
+         } catch (SQLException ex) {
+             Logger.getLogger(DBAccess.class.getName()).log(Level.SEVERE, null, ex);
+         }
+        e.printStackTrace();
+        return false;
+    }
+     return true;
+    }
+    
+    public ResultSet consultar(String sql){
+        ResultSet salida = null;
+        try{
+            Statement st = getConexion().createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+            salida = st.executeQuery(sql);
+        }catch(SQLException e){
+             return null;
+        }     
+        return salida;
+    }
+ 
+}
