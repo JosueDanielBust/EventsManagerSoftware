04e9f195dae07cc1a9a5cafb3ad593f7f282a883
jarangol <julian_arango08@hotmail.com>
2016-05-19 22:07:03 -0500

    Metodos de consultar

--- EVS/src/Main/dbAccess.java
+++ EVS/src/Main/dbAccess.java
@@ -10,14 +10,26 @@ public class dbAccess {
 private Connection conexion;
 
     public static void main(String[] args) {
-        dbAccess obconeccion=new dbAccess();
-        obconeccion.Conectar();
+        dbAccess access = new dbAccess();
+        access.conectar();
+        ResultSet resultados = access.consultar("SELECT CITY_NAME FROM CITY");        
+        if (resultados != null) {
+            try {
+                System.out.println("CIUDAD");
+                System.out.println("------------");
+                while (resultados.next()) {
+                    System.out.println(resultados.getString(1));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
     
     public Connection getConexion() { return conexion; }
     public void setConexion(Connection conexion) { this.conexion = conexion; }
     
-    public dbAccess Conectar() {
+    public dbAccess conectar() {
         try {
             Class.forName("oracle.jdbc.OracleDriver");
             String DB = "jdbc:oracle:thin:@dbeafit.cyzd3byk9uno.us-east-1.rds.amazonaws.com:1521:DB20161";
@@ -28,12 +40,13 @@ private Connection conexion;
             } else {
                 System.out.println("Conexion fallida");
             }
-            
+            /*
             ResultSet st = conexion.createStatement().executeQuery("SELECT * FROM CITY C INNER JOIN PLACE P ON P.CITY_ID = C.CITY_ID");
             while (st.next()) {
                 Array []data;
                 data.add(st.getString(1), st.getString(2), st.getString(3), st.getString(4), st.getString(5), st.getString(6));
             }
+            */
         } catch(Exception e){ e.printStackTrace(); }
     return this;
     }
@@ -47,17 +60,18 @@ private Connection conexion;
      }catch(SQLException e){
         e.printStackTrace();
         return false;
-        }
+    }
      return true;
     }
     
-    public ResultSet Consultar(String sql){
+    public ResultSet consultar(String sql){
         ResultSet salida=null;
         try{
-            Statement st = getConexion().createStatement();
+            Statement st = getConexion().createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
             salida = st.executeQuery(sql);
         }catch(SQLException e){
             e.printStackTrace();
+             return null;
         }
         
         return salida;
